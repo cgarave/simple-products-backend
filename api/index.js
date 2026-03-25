30a8a1ad29ff87868df343e831b9f4c83f94fb57
Rave Flores <reyvflores40@gmail.com>
2026-03-25 09:27:38 +0800

    new item model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import app from '../src/app.js';
 
 // for local testing
-// app.listen(process.env.PORT,
-//     () => console.log(`Listening on port ${process.env.PORT}`));
+app.listen(process.env.PORT,
+    () => console.log(`Listening on port ${process.env.PORT}`));
 
 export default app;
--- src/models/Item.js
+++ src/models/Item.js
@@ -1,12 +1,39 @@
 import mongoose from 'mongoose';
 
 const ItemSchema = new mongoose.Schema({
+    id: mongoose.Types.ObjectId,
     groupName: String,
     itemName: String,
-    retailPrice: Number,
-    wholesalePrice: Number,
-    basePrice: Number,
-    id: mongoose.Types.ObjectId,
+    variation1: {
+        variationName: String,
+        retailPrice: Number,
+        wholesalePrice: Number,
+        basePrice: Number,
+    },
+    variation2: {
+        variationName: String,
+        retailPrice: Number,
+        wholesalePrice: Number,
+        basePrice: Number,
+    },
+    variation3: {
+        variationName: String,
+        retailPrice: Number,
+        wholesalePrice: Number,
+        basePrice: Number,
+    },
+    variation4: {
+        variationName: String,
+        retailPrice: Number,
+        wholesalePrice: Number,
+        basePrice: Number,
+    },
+    variation5: {
+        variationName: String,
+        retailPrice: Number,
+        wholesalePrice: Number,
+        basePrice: Number,
+    }
 })
 
 export default mongoose.model('Items', ItemSchema)
